6b487ff3f000398de5fb960b0cfe95371139144f
Dustin Geletko <dustin.geletko@nasa.gov>
2024-06-14 13:41:44 -0400

    Added fractional scaling to 4k monitors in qtile autostart

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+for screen in $(xrandr | awk '/\<connected\>.*\<3840x/ {print $1}'); do
+    xrandr --output $screen --scale 0.85x0.85
+done
+
 picom --config ~/.config/picom/picom.conf &
 lxpolkit &
 light-locker &
